de658e91aa6c19dcb1d361f23a8816d18fcf53e4
WebMohit20 <2003mohitkhemani@gmail.com>
2024-09-16 12:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+let btn = document.querySelector("input");
+
+let item = document.getElementById("colorSelect") 
+btn.addEventListener("click", ()=>{
+	item.options[item.selectedIndex].remove();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
